91d09baec2f81ea1a2987658405420471d70c965
ManavSharma142 <manav.acno1@gmail.com>
2025-07-03 15:24:52 +0530

    Update BuildTree.cpp

--- BinaryTree/BuildTree.cpp
+++ BinaryTree/BuildTree.cpp
@@ -45,3 +45,60 @@ class Solution {
         return ans;
     }
 };
+
+
+
+
+
+
+// from inorder and postorder
+/* Tree node structure
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* solve( map<int, int>& mp, vector<int> &inorder, vector<int> &postorder, int& index, int InOrderStart, int InOrderEnd, int n) {
+        // base case
+        if(index < 0 || InOrderStart > InOrderEnd) return nullptr;
+        
+        int element = postorder[index--];
+        Node* root = new Node(element);
+        
+        int pos = mp[element];
+
+        
+        root->right = solve(mp, inorder, postorder, index, pos+1, InOrderEnd, n );
+        root->left = solve(mp, inorder, postorder, index, InOrderStart, pos-1, n);
+        return root;
+        
+    }
+    Node *buildTreeUtil(vector<int> &inorder, vector<int> &postorder) {
+        // code here
+        int postOrderIndex = postorder.size()-1;
+        int n = inorder.size();
+        
+        map<int, int> mp ; // node tom index
+        for(int i = 0; i < n; i++) {
+            mp[inorder[i]] = i;
+        }
+        
+        Node* ans = solve(mp, inorder,postorder , postOrderIndex, 0, inorder.size()-1, n);
+        return ans;
+    }
+    // Function to return a tree created from postorder and inoreder traversals.
+    Node* buildTree(vector<int> inorder, vector<int> postorder) {
+        // code here
+        return buildTreeUtil(inorder, postorder);
+    }
+};
