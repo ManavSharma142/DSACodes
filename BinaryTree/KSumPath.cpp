9a6464c67f800a409964ed079afb223774cf8c3c
ManavSharma142 <manav.acno1@gmail.com>
2025-07-05 21:22:38 +0530

    Create KSumPath.cpp

---
+++ BinaryTree/KSumPath.cpp
@@ -0,0 +1,36 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    void solve(TreeNode* root, int key, vector<int>& path, int& count) {
+        if(!root) return;
+
+        path.push_back(root->val);
+
+        solve(root->left, key, path, count);
+        solve(root->right, key, path, count);
+
+        long long sum = 0;
+        for(int i = path.size()-1; i >= 0; i--) {
+            sum += path[i];
+            if(sum == key) count++;
+        }
+        path.pop_back();
+    }
+     int pathSum(TreeNode* root, int targetSum) {
+        vector<int> path;
+        int count = 0;
+        solve(root, targetSum, path, count);
+        return count;
+    }
+};
