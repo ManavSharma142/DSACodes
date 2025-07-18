76ba52c34d49f0d7b624c04167805066fbafc430
ManavSharma142 <manav.acno1@gmail.com>
2025-07-18 20:33:57 +0530

    Update AlienDict.cpp

--- Graph/AlienDict.cpp
+++ Graph/AlienDict.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 class Solution {
   public:
-    bool dfs(unordered_map<int, list<int>>& adj,
-             vector<int>& state, stack<int>& st, int node) {
+    bool dfs(unordered_map<int, list<int>>& adj, vector<int>& state, stack<int>& st, int node) {
         state[node] = 1;
         for (auto i : adj[node]) {
+            // 2nd case: if there is a cycle, return false
             if (state[i] == 1) return false;
             if (state[i] == 0) {
                 if (!dfs(adj, state, st, i)) return false;
@@ -47,7 +47,8 @@ class Solution {
         for (int i = 0; i < n - 1; i++) {
             string s1 = words[i];
             string s2 = words[i + 1];
-
+            
+            // 1st case: if s1 is a prefix of s2 and s1 is longer than s2, return empty string
             if (s1.length() > s2.length() && s1.substr(0, s2.length()) == s2)
                 return "";
 
